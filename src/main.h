d9eb45e1ca8ecb5bff7268b6a5dca5ea4d4a353e
Aaylor <loic.runarvot@gmail.com>
2013-04-05 16:16:28 +0200

    added parsing when launching the program

---
+++ src/main.h
@@ -0,0 +1,9 @@
+#ifndef H_MAIN
+#define H_MAIN
+
+#include "utility.h"
+
+int parse_arguments(int, char **);
+
+#endif
+
--- src/utility.h
+++ src/utility.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define BOARD_LENGTH 9
+
 
 /*
  *  Return the int value associated to the character to match to keys of the
@@ -12,6 +14,7 @@
  */
 #define to_key(character) (character-'A')
 
+#define str_cmp(s1,s2) (strcmp(s1,s2) == 0)
 
 #endif
 
