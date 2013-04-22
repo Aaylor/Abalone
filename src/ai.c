e4cc9cd69fcd611465eb32490c9dad11411a1784
Aaylor <loic.runarvot@gmail.com>
2013-04-22 13:10:39 +0200

    Early work in heuristic ai

--- src/ai.c
+++ src/ai.c
@@ -27,3 +27,69 @@ p_move *random_move(board *b, player cur_player)
     return easy_ai_move;
 }
 
+p_move *heuristic_move(board *b, player cur_player)
+{
+    int i, j, ai_p_move_length = 0;
+    int count_marble_ejection = 0, count_push_opponent_marble = 0;
+    p_move *h_move, *ai_possible_movement;
+    p_move **marble_ejection = NULL, **push_opponent_marble = NULL;
+
+    h_move = malloc(sizeof(p_move));
+    ai_possible_movement = possible_movements(b, cur_player, &ai_p_move_length);
+    
+    for (i = 0; i < ai_p_move_length; i++)
+    {
+        /*
+        fprintf(stdout, "COUP : ");
+        for(j = 0; j < (ai_possible_movement + i)->length; j++)
+        {
+            fprintf(stdout, "%s ", *((ai_possible_movement + i)->squares + j));
+        }
+        putchar('\n');
+        */
+        if (does_move_eject_marble(b, (ai_possible_movement + i)))
+        {
+            fprintf(stdout, "HERE I AM\n");
+            if (count_marble_ejection)
+            {
+                p_move **tmp = realloc(marble_ejection, (sizeof(p_move *) * ((count_marble_ejection++) + 1)));
+                if (tmp)
+                    marble_ejection = tmp;
+                else
+                    free(tmp);
+             }
+             else
+             {
+                marble_ejection = malloc(sizeof(p_move *));
+             }
+             *(marble_ejection + count_marble_ejection) = (ai_possible_movement + i);
+        }
+        else if( 1 )    /* if it move an opponent marble */
+        {
+            if (count_push_opponent_marble)
+            {
+                p_move **tmp = realloc( push_opponent_marble,
+                                        (sizeof(p_move *) * ((count_push_opponent_marble++) + 1)));
+                if (tmp)
+                    push_opponent_marble = tmp;
+                else
+                    free(tmp);
+            }
+            else
+            {
+                push_opponent_marble = malloc(sizeof(p_move *));
+            }
+            *(push_opponent_marble + count_push_opponent_marble) = (ai_possible_movement + i);
+        }
+    }
+
+    if ( marble_ejection );
+        /* choose randomly here */
+    else if( push_opponent_marble );
+        /* choose randomly here */
+    else;
+        /* ah */
+
+    return h_move;
+}
+
